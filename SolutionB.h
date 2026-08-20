33c5c8116dce4a107a12df7b03183a5ca1db888e
Dana Yajanis Guillén Trejos <dana.guillen.trejos@est.una.ac.cr>
2026-08-19 18:45:28 -0600

    Feat: Agregar funciones de SolutionB

--- SolutionB.h
+++ SolutionB.h
@@ -5,13 +5,13 @@
 
 using namespace std;
 
-void addProduct(string*& productNames, double*& productPrices, int*& productStock, int& productCount, string name, double price, int qty);
+void addProduct(string*& productNames, double*& productPrices, int*& productStock, int& productCount, string name, double price, int stock);
 int findProduct(string* productNames, int productCount, string name);
 void updateStock(int* productStock, int index, int newQty);
 void priceMinMax(double* productPrices, int productCount, double* minPrice, double* maxPrice);
 void salesMatrix(int productCount);
 void showInventory(string* productNames, double* productPrices, int* productStock, int productCount);
-void freeAll(string* productNames, double* productPrices, int* productStock);
+void freeAll(string*& productNames, double*& productPrices, int*& productStock, int& productCount);
 void removeProduct(string*& productNames, double*& productPrices, int*& productStock, int& productCount, string name);
 
 #endif
